d6a6226d6e2716470a7531bdfca8cc7d91b1dee0
SkyAscendant <82460791+gaurav337@users.noreply.github.com>
2026-01-19 19:47:28 +0530

    Refactor main execution block and clean up code

--- Faiss_implementation.py
+++ Faiss_implementation.py
@@ -14,7 +14,6 @@ clip_index = faiss.IndexFlatIP(CLIP_DIM)
 
 image_paths = []
 
-
 def hash_to_faiss_vector(hex_str):
     return np.frombuffer(bytes.fromhex(hex_str), dtype=np.uint8)
 
@@ -32,38 +31,37 @@ def add_image_to_faiss(image_name,p_hash,w_hash):
     # keep mapping
     image_paths.append(image_name)
 
+if __name__ == "__main__":
 
+    image_hash_path=r"D:\DejaView-main\image_hashes.json"
 
-image_hash_path=r"C:\Users\Tarush Banke\OneDrive\Desktop\Turing Playground\storage\database_code\image_hashes.json"
+    with open(image_hash_path, 'r') as f:
+        data = json.load(f)
+    print(f"Successfully loaded {len(data)} images.\n")
 
-with open(image_hash_path, 'r') as f:
-    data = json.load(f)
-print(f"Successfully loaded {len(data)} images.\n")
-
-counter=1
-for entry in data:
-    image_name = entry.get('name')
-    p_hash = entry.get('phash')
-    w_hash = entry.get('whash')
-    try:
-        add_image_to_faiss(image_name,p_hash,w_hash)
-        print(f"Indexed {counter} : {image_name}")
-        counter+=1
+    counter=1
+    for entry in data:
+        image_name = entry.get('name')
+        p_hash = entry.get('phash')
+        w_hash = entry.get('whash')
+        try:
+            add_image_to_faiss(image_name,p_hash,w_hash)
+            print(f"Indexed {counter} : {image_name}")
+            counter+=1
+            
+        except Exception as e:
+            print(f"Skipped {image_name}: {e}")
         
-    except Exception as e:
-        print(f"Skipped {image_name}: {e}")
-    
 
 
+    print("Total indexed images:", phash_index.ntotal)
 
-print("Total indexed images:", phash_index.ntotal)
+    faiss.write_index_binary(phash_index, "phash.index")
+    faiss.write_index_binary(whash_index, "whash.index")
 
-faiss.write_index_binary(phash_index, "phash.index")
-faiss.write_index_binary(whash_index, "whash.index")
-
-np.save("image_paths.npy", np.array(image_paths))
+    np.save("image_paths.npy", np.array(image_paths))
 
 
-phash_index = faiss.read_index_binary("phash.index")
-whash_index = faiss.read_index_binary("whash.index")
-image_paths = np.load("image_paths.npy").tolist()
+    phash_index = faiss.read_index_binary("phash.index")
+    whash_index = faiss.read_index_binary("whash.index")
+    image_paths = np.load("image_paths.npy").tolist()
